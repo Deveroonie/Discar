a0987c2c135662f975ee823d2fc3b4c83b692a53
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2022-02-06 14:06:51 +0000

    afk system 😎

--- index.js
+++ index.js
@@ -38,3 +38,21 @@ client.login(process.env.token)
 app.listen("8080", () => {
 	console.log("Info > Express up!")
 })
+
+client.on("messageCreate", async msg => {
+	const isafk = await db.get(`${msg.author.id}_afk`)
+  if(isafk === "true") {
+		msg.reply("Welcome back from AFKness!")
+		db.delete(`${msg.author.id}_afk`)
+		db.delete(`${msg.author.id}_afkreason`)
+	} else {
+		const user = msg.mentions.users.first()
+		if(!user) return;
+		const afk = await db.get(`${user.id}_afk`)
+		if(afk === "true") {
+			const afkreason = await db.get(`${user.id}_afkreason`)
+			if(!afkreason) return msg.reply(`${user.tag} is AFK!`)
+			if(afkreason) return msg.reply(`${user.tag} is afk\n\`${afkreason}\``)
+		}
+	}
+})
